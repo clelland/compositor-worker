fe9d23ca82c3bad1c2ee08747e9324f1536077b6
Majid Valipour <majidvp@chromium.org>
2016-07-11 18:24:26 -0400

    Make parallax static work again
    - Wait for proxies to init
    - Use inner scroller instead of document.scrollingElement

--- parallax-static/parallax.js
+++ parallax-static/parallax.js
@@ -5,8 +5,14 @@
     return scope.window;
   };
 
+  // Log on worker by posting message to main.
+  scope.log = isMain() ? console.log : scope.postMessage;
+
+
   scope.tick = function(timestamp) {
-    console.log("tick:"+timestamp);
+
+    log('tick: ' + scroller.scrollTop);
+
     var t = bg.transform;
     t.m42 = Math.round(scroller.scrollTop*0.8);
     bg.transform = t;
@@ -16,24 +22,46 @@
     scope.requestAnimationFrame(tick);
   };
 
+  // Returns a promise which is resolved once proxy is initialized and can be
+  // mutated.
+  scope.awaitProxyInit = function(proxy) {
+    return new Promise(function(resolve, reject) {
+      function check() {
+        log('checking:' + proxy.initialized);
+        if (proxy.initialized)
+          resolve(proxy);
+        else
+          requestAnimationFrame(check);
+      }
+      requestAnimationFrame(check);
+    });
+  }
+
+
   scope.initWorker = function() {
-    console.log("init worker");
+    log("init worker");
 
     self.onmessage = function(e) {
       scope.scroller = e.data[0];
       scope.bg = e.data[1];
       scope.bg2 = e.data[2];
 
-      scope.requestAnimationFrame(tick);
+      var proxyInitPromises = e.data.map(awaitProxyInit);
+      Promise.all(proxyInitPromises).then(function() {
+        scope.requestAnimationFrame(scope.tick);
+      });
     };
   };
 
   scope.initMain = function() {
-    console.log("init main");
+    log("init main");
 
     scope.worker = new CompositorWorker("parallax.js");
+    scope.worker.onmessage = function(e) {
+      console.log(e.data);
+    }
 
-    scope.scroller = new CompositorProxy(document.scrollingElement, ['scrollTop']);
+    scope.scroller = new CompositorProxy(document.getElementById("content"), ['scrollTop']);
     scope.bg = new CompositorProxy(document.getElementById("bg"), ['transform']);
     scope.bg2 = new CompositorProxy(document.getElementById("bg2"), ['transform']);
 
